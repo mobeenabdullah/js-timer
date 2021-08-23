cc145ab9e4ba37eda10ec83330b87ac5fbccb2e6
Mobeen Abdullah <mobeenabdullah@gmail.com>
2021-08-23 10:21:34 +0500

    Timer class created and bind an event on click

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log('hello world');
+class Timer {
+    constructor(durationInput, startButton, pauseButton) {
+        this.durationInput = durationInput;
+        this.startButton = startButton;
+        this.pauseButton = pauseButton;
+
+        this.startButton.addEventListener('click', this.start);
+    }
+
+    start() {
+        console.log('Start clicked');
+    }
+
+    pause() {}
+
+    onDurationChange() {}
+
+    tick() {}
+}
+
+const durationInput = document.querySelector('#duration');
+const startButton = document.querySelector('#start');
+const pauseButton = document.querySelector('#pause');
+
+const timer = new Timer(durationInput, startButton, pauseButton);
